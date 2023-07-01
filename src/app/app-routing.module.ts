b299ab52eb396089adead02b0d1ef26297e7ac58
Jonas Souza <jonasmzsouza@gmail.com>
2023-07-01 19:39:48 -0300

    :up: update: add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { GamePageComponent } from './pages/game-page/game-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: '', component: HomePageComponent, pathMatch: 'full' },
+	{ path: 'games/:id', component: GamePageComponent },
+	{ path: '**', redirectTo: '' },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
